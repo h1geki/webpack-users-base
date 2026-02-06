1f5858947f9ed591306f2941169f11afd059208b
Verschynin Ivan <v21112005@gmail.com>
2026-02-06 18:49:44 +0300

    init login route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import LoginPage from '../pages/LoginPage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
-    <div>
-        Hello World App
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to="/login" replace/>}/>
+          <Route element={<LoginPage/>} path='/login'/>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>  
   )
 }
 
