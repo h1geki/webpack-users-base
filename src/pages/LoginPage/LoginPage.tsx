7a6ddba170bf95fa76d098565b8c05bf78b3d3b6
Verschynin Ivan <v21112005@gmail.com>
2026-02-07 14:57:20 +0300

    create component structure in LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,11 @@
 import { useMutation } from '@tanstack/react-query'
 import { Form, Button, Input, notification } from 'antd'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { loginUser } from '../../features/auth/loginUser'
+import { loginUser } from '../../features/auth/api/loginUser'
+import LoginForm from '../../features/auth/ui/LoginForm'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../features/auth/model/useAuth'
 
 const LoginPageWrapper = styled.div`
     width:100%;
@@ -12,59 +15,14 @@ const LoginPageWrapper = styled.div`
     align-items:center;
 `
 
-const FromName = styled.p`
-    font-size:17px;
-    font-weight:semi-bold;
-`
-const StyledInput = styled(Input)`
-border:1px solid grey;
-border-radius:3px;
-`
-
-const StyledInputPassword = styled(Input.Password)`
-border:1px solid grey;
-border-radius:3px;
-`
-
 const LoginPage = () => {
-    const {mutate,error,isPending} = useMutation({
-        mutationFn:loginUser,
-        onSuccess:(token:string) => {
-            localStorage.setItem("token",token);
-            notification.success({
-                title:'Вход выполнен',
-                description:'Добро пожаловать'
-            })
-        },
-        onError:(error:Error) => {
-            notification.error({
-                title:'Ошибка входа',
-                description:error.message,
-                placement:'topRight'
-            })
-        }
-    })
-    const onFormSubmit = (userData : {userName:string, userPassword:string}) => {
-        mutate(userData)
-    }    
+  const {isAuthenticated} = useAuth()
+  useEffect(() => {
+    isAuthenticated()
+  },[])
   return (
     <LoginPageWrapper>
-        <Form
-            name="basic"
-            style={{width:'100%',maxWidth:350}}
-            onFinish={onFormSubmit}
-        >
-            <FromName>Авторизация</FromName>
-            <Form.Item name="userName" rules={[{required:true, message:'Введите логин'}]}>
-                <StyledInput placeholder='Логин'/>
-            </Form.Item>
-            <Form.Item name="userPassword" rules={[{required:true,message:'Введите пароль'}]}>
-                <StyledInputPassword placeholder='Пароль' />
-            </Form.Item>
-            <Form.Item style={{display:'flex', justifyContent:'flex-end'}}>
-                <Button disabled={isPending} type='primary' htmlType='submit'>Войти</Button>
-            </Form.Item>
-        </Form>
+        <LoginForm/>
     </LoginPageWrapper>
   )
 }
