b9a41a8c34c8375614442c062911bee5493216a7
Verschynin Ivan <v21112005@gmail.com>
2026-02-08 16:15:03 +0300

    add users route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import UsersPage from '../pages/UsersPage/UsersPage'
 
 const queryClient = new QueryClient()
 
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Navigate to="/login" replace/>}/>
           <Route element={<LoginPage/>} path='/login'/>
+          <Route element={<UsersPage/>} path='/users'/>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>  
